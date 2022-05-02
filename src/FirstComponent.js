8b7c447e0a5ba4654a17010ac17b19cb0cac3aa1
dhanuarsanto <dhanuarsanto@gmail.com>
2022-05-02 20:23:20 +0700

    First Component

---
+++ src/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Cat = () => {
+    console.log('first component')
+    return (
+        <Text>Hello, I am your cat!</Text>
+    )
+}
+
+export default Cat
