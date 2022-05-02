f06784a2d92fe6611ccebde8f50663c560471714
dhanuarsanto <dhanuarsanto@gmail.com>
2022-05-02 19:31:13 +0700

    hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/Hello';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
